64e34a5632c2c08ed5aa5a13bb52ea60e66d4867
soumya bansal <10621576@INFARSZC90748.nmumarl.lntinfotech.com>
2016-06-22 15:03:06 +0530

    Tax payer assignment using angularJs and servlet

---
+++ src/main/webapp/libs/taxPayerController.js
@@ -0,0 +1,30 @@
+﻿/// <reference path="angular.min.js" />
+
+var taxPayerModule = angular.module("taxPayerModule", []);
+
+taxPayerModule.controller("taxPayerController", function($scope) {
+
+	$scope.taxPayers = [
+
+	]
+
+	// Adding Tax payer and redirecting to servlet using $http service
+	$scope.AddTaxPayer = function(data) {
+		alert('I am in add function');
+		$http({
+			method : 'POST',
+			url : 'UserController',
+			headers : {
+				'Content-Type' : 'application/json'
+			},
+			data : $scope.user
+		}).success(function(data) {
+			$scope.status = data;
+		});
+	};
+
+	$scope.cancel = function() {
+		$scope.data = {};
+	}
+
+})
